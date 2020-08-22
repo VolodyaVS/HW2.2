d8d07f2509ea7a9bdba4b32bc90ffcf543cd689e
Volodya <volodyavolga@protonmail.ch>
2020-08-22 21:35:29 +0300

    IB is set

--- HW2.2/ViewController.swift
+++ HW2.2/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var mainMonitor: UIView!
+    
+    @IBOutlet weak var redColorValue: UILabel!
+    @IBOutlet weak var greenColorValue: UILabel!
+    @IBOutlet weak var blueColorValue: UILabel!
+    
+    @IBOutlet weak var redSliderOutlet: UISlider!
+    @IBOutlet weak var greenSliderOutlet: UISlider!
+    @IBOutlet weak var blueSliderOutlet: UISlider!
+    
+    
+    // MARK: - Override Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mainMonitor.layer.cornerRadius = 10
     }
-
-
+    
+    // MARK: - IB Actions
+    @IBAction func redSliderAction(_ sender: Any) {
+        redColorValue.text = String(redSliderOutlet.value)
+    }
+    
+//    @IBAction func greenSliderAction(_ sender: Any) {
+//    }
+//    
+//    @IBAction func blueSliderAction(_ sender: Any) {
+//    }
+    
 }
 
